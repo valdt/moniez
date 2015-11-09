923231a1adefdc82c66e6d6817fa4d9320f3e78c
Fredrik Valdt <valdt@kth.se>
2015-11-09 01:44:27 +0100

    MultiThreading and deadly!

--- classes.py
+++ classes.py
@@ -2,7 +2,8 @@ from yahoo_finance import Share
 from datetime import datetime
 import time, itertools, threading
 class Stock:
-    def __init__(self,name,code):
+    def __init__(self,name,code,):
+        self.result = ""
         self.threadList = []
         self.name = name
         self.code = code
@@ -12,27 +13,32 @@ class Stock:
         self.historyHandler()
         self.buy = False
         self.now = float(self.stock.get_price())
-        self.avrage = 0
+        self.average = 0
+        self.high = 0
+        self.low = 0
+        self.trend()
     def historyHandler(self):
-        print("Collecting...")
-        years = ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15"]
+        yearSubfix = int(time.strftime("%Y-%m-%d")[2:4])
+        years = []
+        for i in range(5):
+            years.append(str(yearSubfix-i))
+        years.reverse()
+        years = ["10","11","12","13","14","15"]
         for data in years:
-            print("Data collector name: {}, id: {} started from 20{}-01-1 too the 20{}-12-31").format(self.name,years.index(data),data,data)
+            print("Data collector thread named: {}, id: {} started from 20{}-01-1 too the 20{}-12-31").format(self.name,years.index(data),data,data)
             year = '20'+data
             self.threadList.append(threading.Thread(target=self.historyBuilder, args=(year,)))
             self.threadList[-1].start()
+            time.sleep(0.1)
         while True:
             for thread in self.threadList:
                 thread.join()
             break
-
-
         for data in years:
             key = '20'+data
             self.history.extend(self.historyDic[key])
-        print("Data collection complete.")
 
-    def historyBuilder(self,year):
+    def historyBuilder(self,year): #Notice: This function is made to be threaded
         start = year + '-01-1'
         stop = year + '-12-31'
         self.historyDic[year] = self.stock.get_historical(start, stop)
@@ -40,68 +46,108 @@ class Stock:
     def trend(self):
         closingValues = []
         trendValues = []
-        temp = {} #Just an holding list, not in "real" use.
+        peakDropDic = {} #Just an holding list, not in "real" use.
         peakDate = []
         deltaList = []
-        avragePeakList = []
-        self.avrage = float(self.history[0]['Close'])
+        averagePeakList = []
+        self.average = float(self.history[0]['Close'])
         #Select all the closing values.
         for day in self.history:
-            if day['Close'] != "None" and float(day['Close']) <= self.avrage*2:
+            if day['Close'] != "None":
                 try:
                     closingValues.append((int(float(day['Close'])*float(10)))/float(10))
-                    self.avrage = (self.avrage + float(day['Close']))/len(closingValues)
                 except:
                     print("Error on the {} due to {}".format(day['Date'],day['Close']))
-        #Sort the list and set avrage
-        #for closeValue in closingValues:
-        #    self.avrage += closeValue
-        #self.avrage = self.avrage / (len(closingValues)-1)
-        if self.now < self.avrage - 2:
+        if self.now < self.average - 2:
             self.buy = True
-        #Calculate the avrage peak and low
+        #Calculate the average peak and low
         closingValuesUnique = []
         for closeValue in closingValues:
             if closeValue not in closingValuesUnique:
                 closingValuesUnique.append(closeValue)
         closingValuesUnique.sort()
-        print(closingValuesUnique)
-        token = int(len(closingValuesUnique))
+        token = int(len(closingValuesUnique)) -1
+        if token <= 11:
+            self.drop = sum(closingValuesUnique[0:1])/2
+            self.peak = sum(closingValuesUnique[token-2:token])/2
+            self.low = closingValuesUnique[2]
+            self.high = closingValuesUnique[-2]
+        elif token <= 101:
+            self.drop = sum(closingValuesUnique[0:10])/10
+            self.peak = sum(closingValuesUnique[token-10:token])/10
+            self.low = closingValuesUnique[10]
+            self.high = closingValuesUnique[token-10]
+        else:
+            self.drop = int(sum(closingValuesUnique[int(token*0.15):int(token*0.35)])/ int(token*0.2))
+            self.peak = int(sum(closingValuesUnique[int(token*0.7):int(token*0.9)])/ int(token*0.2))
+            self.low = closingValuesUnique[int(token*0.1)]
+            self.high = closingValuesUnique[int(token*0.9)]
 
-        self.low = sum(closingValuesUnique[int(token*0.6):int(token*0.8)]) / int(token*0.8 - token*0.6)
-        closingValuesUnique.reverse()
-        self.peak = sum(closingValuesUnique[int(token*0):int(token*0.2)]) / int(token*0.2 - token*0)
-        print(self.low)
-        print(self.peak)
+        #print("{} High:{} Low:{} Peak:{} Drop:{}".format(self.name,self.high,self.low,self.peak,self.drop))
         #Going through all closing values to find dates where stock is in the peak.
         inPeak = False
         inLow = False
+        self.lastPeakorDrop = "none"
         for day in self.history:
             if float(day['Close']) >= self.peak and inPeak == False:
-                temp[day['Date']] = ['peak',day['Close']]
+                peakDropDic[day['Date']] = ['peak',day['Close']]
                 inLow = False
                 inPeak = True
-            elif float(day['Close']) <= self.low and inLow == False:
-                temp[day['Date']] = ['low',day['Close']]
+                self.lastPeakorDrop = "drop"
+            elif float(day['Close']) <= self.drop and inLow == False:
+                peakDropDic[day['Date']] = ['low',day['Close']]
                 inLow = True
                 inPeak = False
-        for date,data in temp.items():
-            print("On the {} {} closed at {} resulting in entering a {}".format(date,self.name,data[1],data[0]))
-        #date_format = "%Y-%m-%d"
+                self.lastPeakorDrop = "peak"
+        dateList = []
+        for date,data in peakDropDic.items():
+            dateList.append(date)
+        dateList.sort()
+        self.averageTime = 0
+        date_format = "%Y-%m-%d"
+        timeList = []
+        i = 0
+        #print(dateList)
+        for n in range(len(dateList)/2):
+            try:
+                a = datetime.strptime(dateList[i], date_format)
+                b = datetime.strptime(dateList[i+1], date_format)
+            except:
+                pass
+            if a > b:
+                timeList.append((a-b).days)
+            elif b > a:
+                timeList.append((b-a).days)
+        self.lowData = True
+        if len(timeList) >= 10:
+            self.lowData = False
+        i = 0
+        for value in timeList:
+            i += value
+        self.averageTime = i/len(timeList)
+        a = datetime.strptime(dateList[-1], date_format)
+        #b = datetime.strptime(time.strftime("%Y-%m-%d"), date_format)
+        b = datetime.strptime(self.historyDic['2015'][0]['Date'], date_format)
+        self.timeInPeriod = (b-a).days
+
+        #for date,data in peakDropDic.items():
+        #    print("On the {} {} closed at {} resulting in entering a {}".format(date,self.name,data[1],data[0]))
+        self.result = ("{} ocsilates at {} days with an average peak at {} and drop at {} we are currently {} from next {} and currently at {}".format(self.name,self.averageTime,self.peak,self.drop,self.timeInPeriod,self.lastPeakorDrop,self.now))
+        #print (time.strftime("%Y-%m-%d"))
         #print ("The stock reach its peak at following dates: {}".format(peakDate))
         #i = 0
         #for n in range(len(peakDate)-1):
         #    try:
         #        print("{} and {}".format(peakDate[0+i],peakDate[1+i]))
-        #        a = datetime.strptime(peakDate[0+i], date_format)
+        #        a =
         #        b = datetime.strptime(peakDate[1+i], date_format)
-        #        avragePeakList.append((a-b).days)
+        #        averagePeakList.append((a-b).days)
         #        i += 2
         #    except:
         #        pass #Out of range on uneven lists.
         #totaltDays = 0
-        #print(avragePeakList)
-        #for days in avragePeakList:
+        #print(averagePeakList)
+        #for days in averagePeakList:
         #    totaltDays += days
-        #self.avragePeak = (totaltDays/(len(avragePeakList)-1))
-        #print(self.avragePeak)
+        #self.averagePeak = (totaltDays/(len(averagePeakList)-1))
+        #print(self.averagePeak)
--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 from yahoo_finance import Share
 from classes import *
+import time, threading
 
 allStocks = {}
 allStocks["Volvo-B"] = 'VOLV-B.ST'
+allStocks["Google"] = 'GOOG'
+allStocks["Apple"] = 'AAPL'
+allStocks["Facebook"] = 'FB'
+allStocks["Audi"] = 'AUDVF'
+#allStocks["Samsung Korea"] = '005930.KS'
 stockClasses = {}
+result = []
+mainThreadList = []
+def boot(name):
+    stockClasses[name] = Stock(name,code)
 for name,code in allStocks.iteritems():
-    if Share(code).get_price() != "None":
-        stockClasses[name] = Stock(name,code)
-    stockClasses["Volvo-B"].trend()
+        mainThreadList.append(threading.Thread(target=boot,args=(name,)))
+        mainThreadList[-1].start()
+        time.sleep(1.5)
+while True:
+    for thread in mainThreadList:
+        thread.join()
+    break
+for name,code in allStocks.iteritems():
+        mainThreadList = []
+        mainThreadList.append(threading.Thread(target=stockClasses[name].trend(),args=()))
+        mainThreadList[-1].start()
+while True:
+    for thread in mainThreadList:
+        thread.join()
+    break
+print("\n")
+for stock in stockClasses:
+    print stockClasses[stock].result
+    time.sleep(0.1)
+    print("")
+print("Fredrik Valdt GNU3.0")
